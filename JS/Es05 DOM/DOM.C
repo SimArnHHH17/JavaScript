d332f6ab4e1bb6b3e2055721b46525cff36adacc
SimArnHHH <simarnhhh@gmail.com>
2024-04-29 16:46:00 +0000

    DOM

---
+++ JS/Es05 DOM/DOM.C
@@ -0,0 +1,30 @@
+/*Script 1: Log delle proprietà dell'oggetto window Questo script stampa le proprietà principali dell'oggetto window nel console, tra cui:
+
+innerHeight e innerWidth: l'altezza e la larghezza dell'area di contenuto della finestra
+outerHeight e outerWidth: l'altezza e la larghezza della finestra, inclusi toolbar e bordi
+location: l'oggetto location della finestra
+history: l'oggetto history della finestra
+document: l'oggetto document della finestra
+navigator: l'oggetto navigator della finestra
+
+Script 2: Log delle proprietà dell'oggetto navigator Questo script stampa le proprietà principali dell'oggetto navigator nel console, tra cui:
+
+userAgent: la stringa dell'agente utente del browser
+appName: il nome del browser
+appVersion: la versione del browser
+platform: la piattaforma (sistema operativo) del browser
+language: la lingua del browser
+onLine: un booleano che indica se il browser è online
+
+Script 3: Stile dei pulsanti Questo script seleziona tutti i button della pagina e 
+imposta il colore di sfondo a arancione e il colore del testo a bianco utilizzando la proprietà style.
+
+Script 4: Testo in maiuscolo di una casella di testo Questo script seleziona la casella di testo con id myInputField e aggiunge un listener di eventi input. 
+Quando l'utente digita del testo, il testo viene convertito in maiuscolo utilizzando il metodo toUpperCase().
+
+Script 5: Tabella HTML dinamica Questo script crea una tabella HTML dinamica contenente la tabella pitagorica 10x10 utilizzando i metodi createElement e appendChild.
+
+Script 6: Aggiunta della classe "evidenziato" alle righe dispari Questo script seleziona tutte le righe della tabella creata nello script 5 e 
+aggiunge la classe evidenziato alle righe dispari utilizzando il metodo setAttribute.
+La classe evidenziato è definita nello stile CSS della pagina e imposta lo sfondo della riga a grigio chiaro.
+*/
